97ed9dae6ee7a20a828b76e1064f320ba1d30d6e
ypareoo <60361371+ypareoo@users.noreply.github.com>
2026-04-24 18:20:04 +0200

    adaptation pour usage depuis Qt

--- RS_ECC/src/main.cpp
+++ RS_ECC/src/main.cpp
@@ -1,56 +1,39 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 #include "RS_tools.hpp"
-#include "RS_Encoder.hpp"
 #include "RS_Decoder.hpp"
 
-int main() {
+#define pb push_back 
+
+int main(int argc,char** argv) {
     int m = 3;
     int n  = (1 << m) - 1;
     int k = 3;
     int t = (n - k) / 2;
 
-    std::cout << "RS(" << n << ", " << k << ") code, t = " << t << std::endl;
-
     GaloisField gf(m);
-    gf.define_generator(n - k);
+    RS_Decoder dec(n, k, gf); 
 
-    RS_Encoder encoder(n, k, gf);
-    RS_Decoder decoder(n, k, gf);
 
-    // Message aléatoire
-    std::vector<int> message(k);
-    for (int i = 0; i < k; ++i) {
-        message[i] = i+1; // Juste pour avoir des valeurs non nulles
-    }
 
-    std :: cout << "Message original : " << std::endl;
-    for(auto x : message) std::cout << x << " ";
-    std::cout << std::endl;
-    
-    std::vector<int> codeword;
-    encoder.encode(message, codeword);
+    for (int i = 1; i < argc; ++i) {
+        unsigned char *conv_in = reinterpret_cast <unsigned char*> (argv[i]);
+        std :: vector <int> received_msg;
+        for(int j = 0 ; conv_in[j] ; ++j){
+            received_msg.pb(conv_in[j] - 48);
+        }
 
-std::cout << "Codeword encodé : " << std::endl;
+        // error injection
 
-    for(int i = 0; i < n; ++i) {
-        std::cout << codeword[i] << " ";
-    }
+        //received_msg[1] = 1;
 
-    std :: cout << std::endl;
+        //std :: cout << "AFFICHAGE Du MESSAGE RECU : " << std :: endl; 
+        //for(auto x : received_msg)
+            //std :: cout << x << " "; 
 
-    // Ajout d'erreurs
-    std::vector<int> received = codeword;
-    received[0 + (n - k)] = 0;
-    received[1] = 0;
-
-    std :: cout << "Message reçu (avec erreurs) : " << std::endl;
-    for(auto x : received) std::cout << x << " ";
-    std::cout << std::endl;
-
-    // Décodage
-    std::vector<int> decoded= decoder.decode(received);
-    std :: cout << "Message décodé : " << std :: endl ;
-    for(auto x : decoded) std::cout << x << " ";
-    std::cout << std::endl;
+        std :: vector <int> decoded_msg = dec.decode(received_msg);
+        //std :: cout << "AFFICHAGE Du MESSAGE DECODER : " << std :: endl; 
+        for(auto x : decoded_msg)
+            std :: cout << x << " "; 
+        std :: cout << std :: endl;
+    }   
 }
